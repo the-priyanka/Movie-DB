38da791a2f05d70d5707932e401a2340acbf1828
Priyanka <original.manaria@gmail.com>
2022-03-05 11:01:59 +0530

    API key

--- src/context.js
+++ src/context.js
@@ -1,4 +1,6 @@
 import React, { useContext } from "react";
+export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`;
+console.log(API_ENDPOINT);
 
 const AppContext = React.createContext();
 
